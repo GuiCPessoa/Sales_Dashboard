d62493779f8b8004af1f97f79cf663aeeaef534d
Guilherme Pessoa <guipessoagith@gmail.com>
2024-12-05 11:09:28 -0300

    Global theme settings and folder structure

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+    black: '#2A2A2B',
+    blue: '#1CA7D8',
+
+    'red-100': '#D72F59',
+    'red-200': '#FFAFC3',
+
+    'white-100': '#FBFBFB',
+    'white-200': '#F4F1f1',
+    
+    'gray-100': '#7C7C8A'
+} as const
